188f0bba72c66d832dfce4b70c1bfd3dfb626b4d
RafaelMSuarez <rafael.martinez1601@alumnos.ubiobio.cl>
2023-11-25 18:51:23 -0300

    se agregan mensajes de salida

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <chrono>
 
 
 using namespace cv;
@@ -13,24 +14,40 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
 	if(argc != 2) {
-		cout << "Debe agregar una imagen" << "\n" ;
+		cerr << "Debe agregar una imagen" << endl ;
 		return 1;
 	}
+	cout << "Cargando imagen ..." << endl;	
+	Mat imagen = imread(argv[1], IMREAD_COLOR);
 
-	Mat image = imread(argv[1], IMREAD_COLOR);
-	Mat output(image.rows, image.cols, CV_8UC1);
+	cout << "Filas (alto): " << imagen.rows << " Columnas (ancho): " << imagen.cols << endl;
+	
+
+	Mat output(imagen.rows, imagen.cols, CV_8UC1);
 
 	uchar greyW;
 
-	for(int r = 0; r < image.rows; r ++) {
-		for(int c = 0; c < image.cols; c++) {
-			Point3_<uchar>* p = image.ptr<Point3_<uchar> >(r, c);
+	auto inicio = chrono::steady_clock::now();
+
+	cout << "Comienza conversion ..." << endl;
+
+	for(int r = 0; r < imagen.rows; r ++) {
+		for(int c = 0; c < imagen.cols; c++) {
+			Point3_<uchar>* p = imagen.ptr<Point3_<uchar> >(r, c);
 			// azul (B) -> x, verde(G) -> y, rojo(R) -> z
 			greyW = static_cast<uchar>((p->x) * 0.114 + (p->y) * 0.587 + (p->z) * 0.299);
 			output.at<uchar>(r,c) = greyW;
 		}
 	}
+	cout << "Finaliza conversion ..." << endl;
 
 	imwrite("output.jpg", output);
+
+	auto termino = chrono::steady_clock::now();
+
+	chrono::duration<double> tiempo = termino - inicio;
+
+	cout << "Tiempo de demora: " << tiempo.count() << " segundos" << endl;
+
 	return 0;
 }
