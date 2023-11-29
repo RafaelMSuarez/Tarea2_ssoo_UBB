5634d51e6010034028a0612bf0b7826f8f193393
RafaelMSuarez <rafael.martinez1601@alumnos.ubiobio.cl>
2023-11-29 19:55:25 -0300

    cambios menores

--- threads/grisThreads.cpp
+++ threads/grisThreads.cpp
@@ -1,4 +1,4 @@
-// install vtk hdf5 fmt (para archlinux)
+// install opencv vtk hdf5 fmt (para archlinux)
 // al parecer libopencv-dev es suficiente para Debian
 //  g++ -Wall grisThreads.cpp -o grisThreads `pkg-config --cflags --libs opencv4` -lpthread
 
